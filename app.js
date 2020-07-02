81bbe31d9861bf0b2c4cbece538ef56f94142b76
DanLaZ <lazenberry_daniel@yahoo.com>
2020-07-02 15:38:36 -0400

    getting data from the api. Im currently looping through the data to get a list of resturant names.

--- app.js
+++ app.js
@@ -6,20 +6,26 @@ axios({
     "method":"GET",
     "url":"https://thefork.p.rapidapi.com/restaurants/list",
     "headers":{
-    "content-type":"application/octet-stream",
+    "content-type":"application/json",
     "x-rapidapi-host":"thefork.p.rapidapi.com",
     "x-rapidapi-key":process.env.THE_FORK_API_KEY,
     "useQueryString":true
     },"params":{
     "pageNumber":"1",
     "queryPlaceValueCoordinatesLongitude":"9.189982",
-    "pageSize":"10",
+    "pageSize":"1",
     "queryPlaceValueCoordinatesLatitude":"45.4642035",
     "queryPlaceValueCityId":"348156"
     }
     })
     .then((response)=>{
-      console.log(response)
+
+        let resturants = response.data.data;
+        
+        for (let i = 0; i < resturants.length; i++) {
+            // resturants[i];
+            console.log(resturants[i].name);
+        }
     })
     .catch((error)=>{
       console.log(error)
