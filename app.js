f0e71e8b43d14d07a5f561189eef4e2920467219
DanLaZ <lazenberry_daniel@yahoo.com>
2020-07-02 15:00:43 -0400

    working on getting a response from the api. .env is set up

--- app.js
+++ app.js
@@ -2,21 +2,6 @@ require('dotenv').config();
 const axios = require('axios');
 
 
-// var url = "https://thefork.p.rapidapi.com/restaurants/list" + "/22f072d9a2mshef218d3aa19563dp15e0cejsn0349cdcb46a3";
-
-// axios.get(url).then(function (response) {
-   
-//   console.log('I worked some how');  
-//   console.log(response);
-// })
-// .catch(function (error) {
-//   // handle error
-//   console.log(error);
-// })
-// .finally(function () {
-//   // always executed
-// });
-
 axios({
     "method":"GET",
     "url":"https://thefork.p.rapidapi.com/restaurants/list",
